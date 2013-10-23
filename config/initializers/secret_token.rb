56cf17a14d28f3edc2f1df1c062ad9120406b10a
Seonggwan Lee <seonggwan.lee@gmail.com>
2013-10-24 00:24:26 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddWordLearning::Application.config.secret_key_base = '51f33cf0d4995c290bdff302c106be5fcc847dfbe3ee41c54aa2a32889bd9c28ab2339664b5958114c835bb7f4a3b497eee9900563be59ac9e81ec2215569a0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TddWordLearning::Application.config.session_store :cookie_store, key: '_tdd-word-learning_session'
